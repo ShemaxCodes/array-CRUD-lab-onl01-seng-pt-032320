2fc0b2f49d0d6facf634005ab03db210e6d611ce
Jashema Panapa <jcpanapa@gmail.com>
2020-03-26 13:55:54 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -35,7 +35,7 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  my_array = ["wow"]
+  my_array = ["wow", "Dog", "am", "arrays!"]
   my_array.shift
 end
 
