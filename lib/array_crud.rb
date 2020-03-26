f1e9447ded9b6cb9a9418a339f73127fbc9a5788
Jashema Panapa <jcpanapa@gmail.com>
2020-03-26 13:14:00 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,7 @@ end
 def add_element_to_end_of_array(array, element)
   my_array = ["Cat", "Dog", "Fish", "Cow"]
   my_array(Bird)
-my_array << "Bird"
+  my_array << "Bird"
 end
 
 def add_element_to_start_of_array(array, element)
